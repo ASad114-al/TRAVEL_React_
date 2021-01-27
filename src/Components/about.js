66726659dd31bdac532e3e23756d2cd649180910
Asad114-al <as3ad.94aa@gmail.com>
2021-01-27 23:22:52 +0100

    about+css

--- src/Components/about.js
+++ src/Components/about.js
@@ -2,7 +2,25 @@
 const About = () => {
     return ( 
      
-   <h1>about</h1>
+      <main className='HeaderMain'>
+            
+      <article className='newWay'>
+         
+          <div className='newText'>
+              <div><img src="../img/element2.png" alt=""/></div>
+              <h1>Guides by Thousand <br/> Sunny </h1>
+              <h4>
+              Packed with tips and advice from our <br/> on-the-ground experts, our city guides app <br/> (iOS and Android) is the ultimate resource <br/> before and during a trip.
+              </h4>
+                 <div id='ring'><img src="../img/element.png" alt=""/></div>
+                 <button id='btn'>Download</button>
+          </div>
+          <div>
+          <img src="../img/Thousand3.png" alt=""/>
+          </div>
+      </article>
+
+ </main>
      
       
      );
