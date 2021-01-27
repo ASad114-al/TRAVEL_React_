d3a349c8409394858b35f8b670827efc2566534b
Asad114-al <as3ad.94aa@gmail.com>
2021-01-27 23:15:29 +0100

    Destinations+css

--- src/Components/Destinations.js
+++ src/Components/Destinations.js
@@ -1,8 +1,49 @@
 
 const Destinations = () => {
     return (  
-        <h1>Destinations</h1>
-    );
-}
+         
+                <main className="DesMain">
+                    <section className="DesSection">
+                    <img id="scoarDes" src="../img/element2.png" alt=""/>
+                    <img id="plusDes"  src="../img/element.png" alt=""/>
+        
+                        <article className="Featured">
+                            <h1>Featured destinations</h1>
+                            <h4>View all </h4>
+                        </article>
+                        <article className="photo">
+                            <div className="Raja">
+                                <div className="text">
+                                    <h3>Raja Ampat</h3>
+                                    <h5>Indonesia</h5>
+                                </div>
+                            </div>
+                            <div className="Fan">
+                                <div className="text">
+                                    <h3>Fanjingshan</h3>
+                                 <h5>China</h5>
+                                </div>
+                            </div>
+                            <div className="Vevey">
+                                <div className="text">
+                                    <h3>Vevey </h3>
+                                    <h5>Switzerland</h5>
+                                </div>
+                            </div>
+                            <div className="Skadar">
+                                <div className="text">
+                                <   h3>Skadar Montenegro</h3>
+                                    <h5>Montenegro</h5>
+                                </div>
+                            </div>
+                        </article>
+                    </section>
+                    <img id="scoarDes2"  src="../img/element2.png" alt=""/>
+                </main>
+            );
+        }
+         
+    
+
  
 export default Destinations;
